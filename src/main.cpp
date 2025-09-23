eba80836033c3020141b55f3d1b376f6ce78ed9b
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-23 16:37:19 -0500

    Verificar que el entorno compila y ejecuta las pruebas correctamente.

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+// src/main.cpp
+#include <iostream>
+
+int main()
+{
+    std::cout << "Iniciando Lab 3 Sistema de Gestión Académica iniciado!" << std::endl;
+    return 0;
+}
---
+++ tests/test_main.cpp
@@ -0,0 +1,8 @@
+// tests/test_main.cpp
+#include <gtest/gtest.h>
+
+// Test básico para asegurar que el framework de pruebas funciona.
+TEST(SanityCheck, BasicAssertion)
+{
+    EXPECT_EQ(true, true);
+}
