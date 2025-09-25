f8df966a03c8028903b19e356f90dc0a2fe4a339
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-25 10:10:23 -0500

    Implementar la lógica de registro y busqueda de personas, manejando memoria dinámica (`new`).

--- include/SistemaGestion.h
+++ include/SistemaGestion.h
@@ -30,6 +30,11 @@ public:
 
     // Destructor
     ~SistemaGestion();
+
+    // Metodos de Gestion
+    void registrarAlumno(int id, const std::string &nombres, const std::string &apellidos, int grado);
+    void registrarProfesor(int id, const std::string &nombres, const std::string &apellidos);
+    Persona *getPersona(int id) const;
 };
 
 #endif
--- src/SistemaGestion.cpp
+++ src/SistemaGestion.cpp
@@ -1,5 +1,7 @@
 // src/SistemaGestion.cpp
 #include "SistemaGestion.h"
+#include "Alumno.h"
+#include "Profesor.h"
 
 // Iniciar puntero estatico
 SistemaGestion *SistemaGestion::instance = nullptr;
@@ -26,4 +28,29 @@ SistemaGestion::~SistemaGestion()
     {
         delete t;
     }
+}
+
+void SistemaGestion::registrarAlumno(int id, const std::string &nombres, const std::string &apellidos, int grado)
+{
+    Alumno *nuevoAlumno = new Alumno(id, nombres, apellidos, grado);
+    this->personas.push_back(nuevoAlumno);
+}
+
+void SistemaGestion::registrarProfesor(int id, const std::string &nombres, const std::string &apellidos)
+{
+    Profesor *nuevoProfesor = new Profesor(id, nombres, apellidos);
+    this->personas.push_back(nuevoProfesor);
+}
+
+Persona *SistemaGestion::getPersona(int id) const
+{
+    // Recorrer vector de punteros
+    for (Persona *p : this->personas)
+    {
+        if (p->getId() == id)
+        {
+            return p;
+        }
+    }
+    return nullptr; // Si no se encuentra, retornar null
 }
