51b54665d5a2407ec4ac934088bea7d853e60904
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-25 09:53:58 -0500

    Escribir el test para registrar Alumno y Profesor en la clase SistemaGestion

--- tests/test_sistema_gestion.cpp
+++ tests/test_sistema_gestion.cpp
@@ -1,6 +1,7 @@
 // tests/test_sistema_gestion.cpp
 #include <gtest/gtest.h>
 #include "SistemaGestion.h"
+#include "Alumno.h"
 
 TEST(SistemaGestionSingletonTest, InstanciaUnica)
 {
@@ -14,4 +15,31 @@ TEST(SistemaGestionSingletonTest, InstanciaUnica)
 
     // Si todo va bien, deberia ser la misma direccion de memoria
     EXPECT_EQ(instancia1, instancia2);
+}
+
+TEST(SistemaGestionFuncionalidadTest, RegistroYConsultaPersonas)
+{
+    SistemaGestion *sistema = SistemaGestion::getInstance();
+
+    // Registrar alumnos y profesores
+    sistema->registrarAlumno(101, "Fabricio", "Balarezo", 6);
+    sistema->registrarProfesor(201, "Maribel", "Guevara");
+
+    // Buscar por ID.
+    Persona *personaAlumno = sistema->getPersona(101);
+    Persona *personaProfesor = sistema->getPersona(201);
+
+    // Verificar que se encontro con exito
+    ASSERT_NE(personaAlumno, nullptr);
+    ASSERT_NE(personaProfesor, nullptr);
+
+    // Son del tipo correcto?
+    EXPECT_EQ(personaAlumno->getTipo(), "Alumno");
+    EXPECT_EQ(personaProfesor->getTipo(), "Profesor");
+
+    // Guia: https://www.geeksforgeeks.org/cpp/dynamic-_cast-in-cpp/
+    // Acceder a las funciones de la clase derivada
+    Alumno *alumno = dynamic_cast<Alumno *>(personaAlumno);
+    ASSERT_NE(alumno, nullptr);
+    EXPECT_EQ(alumno->getGrado(), 6);
 }
