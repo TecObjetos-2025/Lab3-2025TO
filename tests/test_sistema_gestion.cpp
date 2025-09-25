9682319a5a1af376ea27ea31d0c86672062585b1
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-25 09:14:18 -0500

    Escribir pruebas para el SistemaGestion y asegurar el patron Singleton

---
+++ tests/test_sistema_gestion.cpp
@@ -0,0 +1,17 @@
+// tests/test_sistema_gestion.cpp
+#include <gtest/gtest.h>
+#include "SistemaGestion.h"
+
+TEST(SistemaGestionSingletonTest, InstanciaUnica)
+{
+    // Obtener la instancia
+    SistemaGestion *instancia1 = SistemaGestion::getInstance();
+
+    ASSERT_NE(instanci1, nullptr);
+
+    // Obteniendo instancia otra vez
+    SistemaGestion *instancia2 = SistemaGestion::getInstance();
+
+    // Si todo va bien, deberia ser la misma direccion de memoria
+    EXPECT_EQ(instancia1, instancia2);
+}
