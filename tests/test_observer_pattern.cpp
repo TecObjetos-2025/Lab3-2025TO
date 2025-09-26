961cdf946390012134006613c0ef639a7260f1f6
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-26 11:27:35 -0500

    Refactor en el test de observer ahora refleja un caso de uso real en el Sistema

--- tests/test_observer_pattern.cpp
+++ tests/test_observer_pattern.cpp
@@ -2,35 +2,19 @@
 #include <gtest/gtest.h>
 #include "Tarea.h"
 #include "Profesor.h"
-#include "Observador.h"
-
-// Mock como "doble"
-class MockProfesor : public Observador
-{
-public:
-    bool notificado = false;
-    int idTareaNotificada = 0;
-
-    void actualizar(Tarea *tarea) override
-    {
-        notificado = true;
-        // Podria guardarse el ID y otra logica, esta es solo prueba
-    }
-};
 
 TEST(ObserverPatternTest, ProfesorEsNotificadoCuandoSeEntregaLaTarea)
 {
-    MockProfesor mockProf;
-    Profesor *profeReal = new Profesor(201, "Maribel", "Guevara");
-    Tarea tarea(1, "Observer Pattern", profeReal, "2025-09-26");
+    Profesor *profe = new Profesor(201, "Maribel", "Guevara");
+    Tarea tarea(1, "Tecnologia de Objetos", profe, "2025-09-26");
 
-    tarea.agregarObservador(&mockProf);
+    tarea.agregarObservador(profe);
 
-    ASSERT_FALSE(mockProf.notificado);
+    ASSERT_EQ(profe->getTareasRecibidas(), 0);
 
     tarea.marcarComoEntregada();
 
-    EXPECT_TRUE(mockProf.notificado);
+    EXPECT_EQ(profe->getTareasRecibidas(), 1);
 
-    delete profeReal;
+    delete profe;
 }
