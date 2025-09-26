2fee16f0d55155da7403f35d3d958b8751e28167
fabrik12 <fbalarezo@unsa.edu.pe>
2025-09-26 11:03:04 -0500

    Escribir prueba para que se verifique que el Profesor es notificado cuando una Tarea cambia de estado

---
+++ tests/test_observer_pattern.cpp
@@ -0,0 +1,36 @@
+// tests/test_observer_pattern
+#include <gtest/gtest.h>
+#include "Tarea.h"
+#include "Profesor.h"
+#include "Observador.h"
+
+// Mock como "doble"
+class MockProfesor : public Observador
+{
+public:
+    bool notificado = false;
+    int idTareaNotificada = 0;
+
+    void actualizar(Tarea *tarea) override
+    {
+        notificado = true;
+        // Podria guardarse el ID y otra logica, esta es solo prueba
+    }
+};
+
+TEST(ObserverPatternTest, ProfesorEsNotificadoCuandoSeEntregaLaTarea)
+{
+    MockProfesor mockProf;
+    Profesor *profeReal = new Profesor(201, "Maribel", "Guevara");
+    Tarea tarea("Observer Pattern", profeReal, "2025-09-26");
+
+    tarea.agregarObservador(&mockProf);
+
+    ASSERT_FALSE(mockProf.notificado);
+
+    tarea.marcarComoEntregada();
+
+    EXPECT_TRUE(mockProf.notificado);
+
+    delete profeReal;
+}
